428db769758441f87694f367e73e2beb9eb8dbb3
MarkAWard <markmaw@gmail.com>
2014-10-20 07:29:37 -0400

    serializable

--- src/edu/nyu/cs/cs2580/DocumentIndexed.java
+++ src/edu/nyu/cs/cs2580/DocumentIndexed.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
  * @CS2580: implement this class for HW2 to incorporate any additional
  * information needed for your favorite ranker.
  */
-public class DocumentIndexed extends Document {
+public class DocumentIndexed extends Document implements Serializable {
     private static final long serialVersionUID = 9184892508124423115L;
     
     private static HashMap <Integer, Integer> _df = new HashMap<Integer,Integer>();
